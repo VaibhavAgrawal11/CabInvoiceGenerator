b6757c0aac4a42a6f76aeea8fc491ffafab79e70
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-04-06 08:49:59 +0530

    ADDED: Welcome message.

---
+++ src/main/java/com/bridgelabz/serivice/CabService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.serivice;
+
+public class CabService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to cab invoice generator problem statement.");
+    }
+}
